df3dc33e118e76cbd70adf723f24931a96eb03ec
Pfeffimann18 <leon_gross@icloud.com>
2023-01-08 00:41:21 +0100

    neovim

---
+++ nvim/lua/leon/core/keymaps.lua
@@ -0,0 +1,13 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
---
+++ nvim/lua/leon/plugins-setup.lua
@@ -0,0 +1,49 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+local status, packer = pcall(require, "packer")
+if not status then
+  return
+end
+
+return packer.startup(function(use)
+  use("wbthomason/packer.nvim")
+  use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use("christoomey/vim-tmux-navigator") -- tmux & split window navigation  
+  use("numToStr/Comment.nvim")  
+  use("nvim-tree/nvim-tree.lua")
+  use("nvim-tree/nvim-web-devicons")
+  use("nvim-lualine/lualine.nvim")
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+  use("hrsh7th/nvim-cmp") -- completion plugin
+  use("hrsh7th/cmp-buffer") -- source for text in buffer
+  use("hrsh7th/cmp-path") -- source for file system paths
+  use("L3MON4D3/LuaSnip") -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets") -- useful snippets
+  use'NvChad/nvim-colorizer.lua'
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+end)
